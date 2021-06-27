04a3c9d72d5b3e34dee4c3654d10d601481b4e35
Mathis Bullinger <mathis@bullinger.dev>
2021-06-28 01:27:08 +0200

    screen box collision

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ type Particle = {
   cl: string
   r: number
   stop: number
+  previous: Vec[]
 }
 
 const ranCl = () =>
@@ -20,12 +21,15 @@ const ranCl = () =>
     .map(() => Math.floor((Math.random() * 200 + 55) / 0xf).toString(16)[0])
     .join('')
 
+const maxV = 300
+
 const genPart = (): Particle => ({
   pos: [Math.random() * canvas.width, Math.random() * canvas.height],
-  vel: [0, 0],
+  vel: [Math.random() * maxV - maxV / 2, Math.random() * maxV - maxV / 2],
   cl: ranCl(),
   r: 10 + Math.random() ** 2 * 200,
   stop: (Math.random() * 0.5) ** 2,
+  previous: [],
 })
 
 const particles: Particle[] = []
@@ -48,11 +52,12 @@ function render() {
     ctx.fillRect(x - r, y - r, r * 2, r * 2)
   }
 
-  const [cx, cy] = [0, 1].map(
-    (i) => particles.reduce((a, c) => a + c.pos[i], 0) / particles.length
-  )
-  ctx.strokeStyle = '#f00'
-  ctx.strokeRect(cx - 10, cy - 10, 20, 20)
+  // center pos
+  // const [cx, cy] = [0, 1].map(
+  //   (i) => particles.reduce((a, c) => a + c.pos[i], 0) / particles.length
+  // )
+  // ctx.strokeStyle = '#f00'
+  // ctx.strokeRect(cx - 10, cy - 10, 20, 20)
 }
 
 let mouse: Vec | null = null
@@ -61,6 +66,7 @@ canvas.addEventListener('mousemove', ({ clientX: x, clientY: y }) => {
 })
 
 let last = performance.now()
+
 function update() {
   const dt = (performance.now() - last) / 1000
   last += dt * 1000
@@ -68,11 +74,31 @@ function update() {
   for (const part of particles) {
     for (const p2 of particles) {
       if (p2 === part) continue
-      attract(p2, part.pos, dt, part.r / 210)
+      attract(p2, part.pos, dt, part.r ** 2 / 50000)
     }
 
     part.pos[0] += part.vel[0] * dt
     part.pos[1] += part.vel[1] * dt
+
+    // friction
+    // part.vel[0] *= 1 - 0.1 * dt
+    // part.vel[1] *= 1 - 0.1 * dt
+
+    // collision
+    if (part.pos[0] < 0) {
+      part.pos[0] = 0
+      part.vel[0] *= -1
+    } else if (part.pos[0] > canvas.width) {
+      part.pos[0] = canvas.width
+      part.vel[0] *= -1
+    }
+    if (part.pos[1] < 0) {
+      part.pos[1] = 0
+      part.vel[1] *= -1
+    } else if (part.pos[1] > canvas.height) {
+      part.pos[1] = canvas.height
+      part.vel[1] *= -1
+    }
   }
 }
 
@@ -81,7 +107,7 @@ function attract(part: Particle, origin: Vec, dt: number, m = 1) {
     (origin[0] - part.pos[0]) ** 2 + (origin[1] - part.pos[1]) ** 2
   )
   if (d === 0) return
-  const f = Math.min(m / (d / 1000) ** 2, 500)
+  const f = Math.min(m / (d / 1000) ** 2, 1000)
   const fx = ((origin[0] - part.pos[0]) / d) * f
   const fy = ((origin[1] - part.pos[1]) / d) * f
   part.vel[0] += fx * dt
